cee97cddd480958f11771699716671db4b76ab4f
Dave <david.j.anderson94@gmail.com>
2019-03-19 20:50:04 -0700

    added postgresql to production.py

--- django_project/settings/production.py
+++ django_project/settings/production.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = ['app.django-blog.com']
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_db',
+        'USER': 'app_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
