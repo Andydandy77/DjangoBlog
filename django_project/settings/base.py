0c411b8529acaaf6ba26df1112f9f6f959d3a777
Dave <david.j.anderson94@gmail.com>
2019-03-24 19:59:43 -0700

    reverted back to old hierarchy

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -76,26 +76,13 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DEBUG = False
-ALLOWED_HOSTS = ['app.django-blog.com']
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'app_db',
-        'USER': 'app_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
